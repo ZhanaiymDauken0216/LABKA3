c391312f8c80c9095fcb91dad8cb69367ac56335
Zhaaniym Dauken <zhanaiym.sweet16@gmail.com>
2022-10-04 13:20:42 +0600

    first commit

--- ZhanaiymProekt04/main7.py
+++
@@ -1,9 +0,0 @@
-string = "zhdndiym"
- #заменяет в строке одну
-#подстроку на другую
-
-# replace all instances of 'd' (old) with 'a' (new)
-new_string = string.replace("d", "a" )
- 
-print(string)
-print(new_string)
--- ZhanaiymProekt04/main8.py
+++
@@ -1,5 +0,0 @@
-#разбивает строку на
-#подстроки в зависимости от разделителя
-string = "one,two,three"
-words = string.split(',')
-print(words)
